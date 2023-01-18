115f745a6bf4be8b89c6b77b778e526d619aa782
Jimr77777 <jimroller@gmail.com>
2023-01-18 16:25:13 -0600

    update user get

--- NaboryAPI/Controllers/UserController.cs
+++ NaboryAPI/Controllers/UserController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Persistence.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace NaboryAPI.Controllers
@@ -12,19 +16,36 @@ namespace NaboryAPI.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly User[] user;
+        private IApplicationDbContext _dbContext;
+
         private readonly ILogger<UserController> _logger;
 
-        public UserController(ILogger<UserController> logger)
+        public UserController(ILogger<UserController> logger, IApplicationDbContext userRepository)
         {
+            _dbContext = userRepository;
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<User> Get()
+        [HttpGet("{Id}", Name = "UserById")]
+        public async Task<IActionResult> Get(int Id)
         {
-            
-            return user;
+            try
+            {
+                var user = await _dbContext.Users.FindAsync(Id);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+          
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(User _user)
+        {
+            //await _dbContext.Users.AddAsync();
+            return Ok(_user);
         }
 
     }
---
+++ NaboryAPI/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace NaboryAPI
+{
+    public interface IUserRepository
+    {
+        public Task<User> GetByIdAsync(int id);
+    }
+}
